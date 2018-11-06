43df17a32db75ac93b14da78299ab655991635ca
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2018-11-06 20:54:32 +0100

    Add DataException

---
+++ Cynov/DataException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cynov
+{
+    class DataException : Exception
+    {
+        public DataException(string message) : base(message) { }
+    }
+}
